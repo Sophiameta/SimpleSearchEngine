4ed85037214a4524c756be9622bbae999ea4f0c2
Lyserg25 <Lyserg25@users.noreply.github.com>
2015-04-22 19:43:44 +0200

    Update crawl.py
    gibt den output:
    
    d05:d04
    d08:
    d03:d04,d01,d02,d05
    d04:d01,d02,d03,d05
    d01:d02,d03,d04
    d02:d03,d04,d01,d05
    d06:d07
    d07:d06
    
    also schonmal richtig, nur die reihenfolge stimmt nicht..kp ob ihm das wichtig ist

--- crawl.py
+++ crawl.py
@@ -1,30 +1,50 @@
-__author__ = 's0540017'
+__author__ = 's0539720'
 
 from urllib.parse import urlparse
-import urllib
+import urllib.request
 from bs4 import BeautifulSoup
 
-url = "http://people.f4.htw-berlin.de/fileadmin/user_upload/Dozenten/WI-Dozenten/Classen/DAWeb/smdocs/d01.html"
 
-urls = [url]
-visited = [url]
+seed = ["http://people.f4.htw-berlin.de/fileadmin/user_upload/Dozenten/WI-Dozenten/Classen/DAWeb/smdocs/d01.html",
+        "http://people.f4.htw-berlin.de/fileadmin/user_upload/Dozenten/WI-Dozenten/Classen/DAWeb/smdocs/d06.html",
+        "http://people.f4.htw-berlin.de/fileadmin/user_upload/Dozenten/WI-Dozenten/Classen/DAWeb/smdocs/d08.html"]
+urls = [] #URLs that have to be visited
+visited = [] #already visited URLs
+linksOnSite = {} #title of site as key, links of the site as values
+titleOfSite = {} #url as key, title of the site as value
 
-htmltext = ""
+#crawl every URL in seed list + URLs found on the websites
+for url in seed:
+    urls.append(url)
+    visited.append(url)
 
-while len(urls) > 0:
-    try:
-        htmltext = urllib.urlopen(urls[0]).read()
-    except:
-        print(urls[0])
-    soup = BeautifulSoup(htmltext)
+    while len(urls) > 0:
+        try:
+            website = urllib.request.urlopen(urls[0]).read()
+        except:
+            print("Could not crawl the following website: " + urls[0])
 
-    urls.pop(0)
-    print(len(urls))
+        soup = BeautifulSoup(website)
+        title = soup.title.string
+        linksOnSite[title] = []
+        titleOfSite[urls[0]] = title
+        urls.pop(0)
 
-    for tag in soup.findAll('a',href=True):
-        tag['href'] = urlparse.urljoin(url, tag['href'])
-        if url in tag['href'] and tag['href'] not in visited:
-            urls.append(tag['href'])
-            visited.append(tag['href'])
+        for tag in soup.findAll('a',href=True):
+            tag['href'] = urllib.parse.urljoin(url, tag['href'])
+            linksOnSite[title].append(tag['href'])
+            if tag['href'] not in visited:
+                urls.append(tag['href'])
+                visited.append(tag['href'])
 
-print(visited)
+#print the link structure
+for site, links in linksOnSite.items():
+    structure = site + ":"
+    first = True
+    for link in links:
+        if first:
+            first = False
+            structure = structure + titleOfSite[link]
+        else:
+            structure = structure + "," + titleOfSite[link]
+    print(structure)
